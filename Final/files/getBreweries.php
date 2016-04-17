56c3da0a4fcb81b2f3db449c343ae3aecdefd862
mjb236 <mjb236@pitt.edu>
2016-04-17 10:29:52 -0400

    Added brewery search files, moved close body and html tags out of footer.

---
+++ Final/files/getBreweries.php
@@ -0,0 +1,24 @@
+<?php
+	//connect to the database for querying information
+	$server = "localhost";
+	$user = "bowen";
+	$pw = "bowen1234";
+	$dbname = "project";
+	$db = new mysqli($server, $user, $pw, $dbname);
+	if($db->connect_error) {
+		die("Database connection error: " . $db->connect_error);
+	}
+
+	//get brewery names and ID numbers from database
+	$select = "SELECT ID, Name
+						FROM Breweries";
+	$result = $db->query($select);
+	$rows = $result->num_rows;
+	if($rows > 0) {
+		while($row = mysqli_fetch_row($result)) {
+			echo json_encode($row);
+		}
+	}
+
+	$db->close();
+?>
